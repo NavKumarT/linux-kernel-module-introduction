7f93505ad7529d488d4aae3aff13c56ca53ec21d
Naveen Kumar T <76838772+NavKumarT@users.noreply.github.com>
2021-11-05 21:35:02 +0530

    remove include errors and other bugs
    For newer kernels write "proc_ops" instead of "file_operations"

--- simple_2.c
+++ simple_2.c
@@ -1,18 +1,18 @@
 #include<linux/init.h>
-#include<kernel.h>
+#include<linux/kernel.h>
 #include<linux/module.h>
 #include<linux/proc_fs.h>
-#include<asm/access.h>
+#include<asm/uaccess.h>
 
 #define BUFFER_SIZE 128
 #define PROC_NAME "jiffies"
 
 ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos);
 
-static struct file_operations proc_ops = {
+static struct proc_ops proc_ops = {
 	.owner = THIS_MODULE,
 	.read = proc_read,
-}
+};
 
 int proc_init(void)
 {
@@ -25,10 +25,10 @@ void proc_exit(void)
 	remove_proc_entry(PROC_NAME, NULL);
 }
 
-ssize t proc read(struct file *file, char user *usr buf, size t count, loff t *pos)
+ssize_t proc_read(struct file *file, char __user *usr_buf, size_t count, loff_t *pos)
 {
 	int rv = 0;
-	char buffer[BUFFER SIZE];
+	char buffer[BUFFER_SIZE];
 	static int completed = 0;
 	if (completed) {
 	completed = 0;
@@ -37,11 +37,11 @@ ssize t proc read(struct file *file, char user *usr buf, size t count, loff t *p
 	completed = 1;
 	rv = sprintf(buffer, jiffies);
 
-	copy_to_user(usr buf, buffer, rv);
+	copy_to_user(usr_buf, buffer, rv);
 	return rv;
 }
-module init(proc init);
-module exit(proc exit);
-MODULE LICENSE("GPL");
-MODULE DESCRIPTION("Jiffies Module");
-MODULE AUTHOR("SGG");
+module_init(proc_init);
+module_exit(proc_exit);
+MODULE_LICENSE("GPL");
+MODULE_DESCRIPTION("Jiffies Module");
+MODULE_AUTHOR("SGG");
